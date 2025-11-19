789b38d8cea7ceedf6ada3ca402486412e2cda74
Guillermo Barazzutti <gbarazzutti2@gmail.com>
2025-11-19 09:28:41 -0300

    feat: Implement blog and category components, add API integration and update styles

---
+++ src/types/index.ts
@@ -0,0 +1,67 @@
+import { z } from 'astro:content'
+import { array } from 'astro:schema'
+
+const imageSchema = z.object({
+  url: z.string(),
+  width: z.number(),
+  height: z.number()
+})
+
+const featuredImagesSchema = z.object({
+  thumbnail: imageSchema,
+  medium: imageSchema,
+  medium_large: imageSchema,
+  large: imageSchema,
+  full: imageSchema,
+})
+
+export const BaseWPSchema = z.object({
+    id: z.number(),
+    slug: z.string(),
+    title: z.object({
+      rendered: z.string()
+    }),
+    content: z.object({
+      rendered: z.string()
+    }),
+    featured_images: featuredImagesSchema,
+    acf: z.object({
+      subtitle: z.string()
+    })
+})
+
+const processSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string()
+})
+
+export const ProcessPageSchema = BaseWPSchema.extend({
+    acf: z.object({
+        subtitle: z.string()
+    }).catchall(processSchema)
+})
+
+const CategorySchema = z.object({
+    name: z.string(),
+    slug: z.string()
+})
+
+const CategoriesSchema = z.array(CategorySchema)
+
+// Schema para Posts. Podríamos copiar el BaseWPSchema y quitar el campo de ACF, pero seria duplicar código.
+// Heredamos todo el contenido de BaseWPSchema y omitimo el campo 'acf'
+
+export const PostSchema = BaseWPSchema.omit({
+    acf: true
+}).extend({
+    date: z.string(),
+    category_details: CategoriesSchema
+})
+
+// Ahora hay un problema, con esto traemos solo un post. Porque el esquema base parte desde un objeto (z.object)
+// Vamos a crear un arreglo con el PostSchema para que traiga todos los posts
+
+export const PostsSchema = z.array(PostSchema)
+
+export type Post = z.infer<typeof PostSchema>
